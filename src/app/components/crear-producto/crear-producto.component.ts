5df71caa3549abcd7c3251c6d108804700b70566
Juan camilo Villacob <107330476+jcvillacob@users.noreply.github.com>
2023-01-19 22:42:48 +0000

    3 commit

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-producto',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-producto.component.css']
 })
 export class CrearProductoComponent {
+  productoForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required], 
+      categoria: ['', Validators.required], 
+      ubicacion: ['', Validators.required], 
+      precio: ['', Validators.required], 
+    })
+  }
+
+  agregarProducto(){
+    console.log(this.productoForm)
+  }
 
 }
