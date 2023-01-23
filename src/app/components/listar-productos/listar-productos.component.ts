d7ea62cb63eb64727e98da5b78df41876f238618
Juan camilo Villacob <107330476+jcvillacob@users.noreply.github.com>
2023-01-23 20:05:33 +0000

    dafbgsdf

--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-listar-productos',
   templateUrl: './listar-productos.component.html',
   styleUrls: ['./listar-productos.component.css']
 })
-export class ListarProductosComponent {
+export class ListarProductosComponent implements OnInit {
 
+  listProductos: any[] = [
+    {
+      "_id": "63cecdc444b14713c7080f9f",
+      "nombre": "Pepsi",
+      "categoria": "bebidas",
+      "ubicacion": "Barcelona",
+      "precio": 5,
+      "fechaCreacion": "2023-01-23T18:07:44.058Z",
+      "__v": 0
+    },
+    {
+      "_id": "63cecef192f213aa33111f3f",
+      "nombre": "Fantassss",
+      "categoria": "bebidas",
+      "ubicacion": "Barcelona",
+      "precio": 10,
+      "fechaCreacion": "2023-01-23T18:15:43.749Z",
+      "__v": 0
+    },
+    {
+      "_id": "63ced41a7a89ffc7ff515baf",
+      "nombre": "Coca Cola",
+      "categoria": "bebidas",
+      "ubicacion": "Barcelona",
+      "precio": 10,
+      "fechaCreacion": "2023-01-23T18:38:05.117Z",
+      "__v": 0
+    }
+  ]
+  
+  constructor(private _productoService: ProductoService){}
+  
+  ngOnInit(): void {
+    //this.obtenerProductos();
+  }
+
+  obtenerProductos(){
+    this._productoService.getProductos().subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    })
+  }
 }
